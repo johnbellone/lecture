889881e0bff27082b9fc94592593041e3dfcc550
John Bellone <john.bellone.jr@gmail.com>
2009-11-10 23:06:22 -0500

    Fixed bitfield example

--- c++/bitfield.cpp
+++ c++/bitfield.cpp
@@ -2,26 +2,52 @@
 #include <iostream>
 
 typedef struct {
-	int id : 3;
-	char tag : 5;
+	unsigned char x : 3;
+	unsigned char y : 5;
 } A;
 
 typedef struct {
-	int id;
-	char tag;
+	unsigned char x;
+	unsigned char y;
 } B;
 
 typedef struct {
-	A t1;
-	unsigned char tag : 5;
-} C; 
+	A x;
+	unsigned char y : 5;
+} C;
+
+typedef struct {
+	unsigned int x : 3;
+	unsigned int y : 5;
+} D;
+
+typedef struct {
+	unsigned int x : 3;
+	unsigned char y : 5;
+} E;
+
+typedef struct {
+	unsigned int x : 3;
+	unsigned int y : 5;
+	unsigned int z : 5;
+} F;
+
+typedef struct {
+	unsigned int x : 3;
+	unsigned int y : 5;
+	unsigned int z;
+} G;
 
 int
 main (int argc, char * argv[]) {
-
-	std::cout << "Size of unsigned int (bytes): " << sizeof (unsigned int) << "\n";
-	std::cout << "Size of unsigned char (bytes): " << sizeof (unsigned char) << "\n";
-	std::cout << "Size of A (bytes): " << sizeof (A) << "\n";
-	std::cout << "Size of B (bytes): " << sizeof (B) << "\n";
-	std::cout << "Size of C (bytes): " << sizeof (C) << "\n";
+	std::cout << "Size of unsigned int: " << sizeof (unsigned int) << " bytes\n";
+	std::cout << "Size of unsigned char: " << sizeof (unsigned char) << " bytes\n";
+	std::cout << "Size of A: " << sizeof (A) << " bytes\n";
+	std::cout << "Size of B: " << sizeof (B) << " bytes\n";
+	std::cout << "Size of C: " << sizeof (C) << " bytes\n";
+	std::cout << "Size of D: " << sizeof (D) << " bytes\n";
+	std::cout << "Size of E: " << sizeof (E) << " bytes\n";
+	std::cout << "Size of F: " << sizeof (F) << " bytes\n";
+	std::cout << "Size of G: " << sizeof (G) << " bytes\n";
+	
 }
